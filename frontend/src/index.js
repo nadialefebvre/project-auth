f8b68ea6a74918297990d5a9b7bda120bc8122f7
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-05-30 13:49:01 +0200

    removed commented out code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
-// import ReactDOM from 'react-dom/client'
 import {createRoot} from 'react-dom/client'
 
 import './index.css'
 import App from './App'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
